c8e9bd7aa0ba80a5c3a4682f207012ee4c67f3f6
wicky4t <bhaiwicky5@gmail.com>
2025-07-26 00:00:00 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -272,7 +272,7 @@ function App() {
                 </feMerge>
               </filter>
             </defs>
-           <rect width="100%" height="100%" fill="url(#smallGrid)" filter="url(#gridGlow)" opacity="0.7" />
+           <rect width="100%" height="100%" fill="url(#smallGrid)" filter="url(#gridGlow)" opacity="1" />
           </svg>
         </div>
       <FloatingIcons />
